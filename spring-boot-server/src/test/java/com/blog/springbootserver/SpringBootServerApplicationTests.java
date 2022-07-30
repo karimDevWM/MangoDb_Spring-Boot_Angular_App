aad29192c080348d38774d0fb27f8e25ff6aa169
karimDevWM <ka.trabelsi9@gmail.com>
2022-07-30 10:09:27 +0200

    first commit

---
+++ spring-boot-server/src/test/java/com/blog/springbootserver/SpringBootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.springbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
